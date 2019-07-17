201a6ef15b42a4d43473ec48ba5e38703db33396
997959066 <997959066@qq.com>
2019-07-17 20:39:17 +0800

    1

--- src/main/java/cn/xiaoyu/interceptor/MyMvcConfig.java
+++ src/main/java/cn/xiaoyu/interceptor/MyMvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SuppressWarnings("deprecation")
-//@Configuration
+@Configuration
 public class MyMvcConfig extends WebMvcConfigurerAdapter {
 	/**
 	 * 静态资源配置
