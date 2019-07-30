53849a193239aaae518b143b5fe9372687efed84
997959066 <997959066@qq.com>
2019-07-30 18:58:48 +0800

    1

--- src/main/java/cn/xiaoyu/controller/tally/IncomeController.java
+++ src/main/java/cn/xiaoyu/controller/tally/IncomeController.java
@@ -1,9 +1,4 @@
 package cn.xiaoyu.controller.tally;
-import java.util.HashMap;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import cn.xiaoyu.common.Base;
 import cn.xiaoyu.common.MessageCode;
@@ -13,76 +8,84 @@ import cn.xiaoyu.entity.tally.Income;
 import cn.xiaoyu.util.DateUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
 /**
  * 描述:
- * @author  xiaoyu.zhang
+ *
+ * @author xiaoyu.zhang
  * 日期:2018-08-09
  * 日期:2018-12-20 增加详情查询
-*/ 
+ */
 @RestController
 @RequestMapping("/income")
 @Api(value = "income", description = "收入")
 public class IncomeController extends Base {
 
-	@ApiOperation(value = "查询年收入详情", notes = "查询菜单列表")
-	@RequestMapping(value = "/incomeDetails", method = RequestMethod.GET)
-	public ResponseMessage incomeDetails(Income income,Integer pageNo,Integer pageSize){
-		try { 
-			if(income.getYear() == null){
-				income.setYear(Integer.valueOf(DateUtil.getSysYear()));
-			}
-			PageBean<Income> result = incomeService.incomeDetails(income,pageNo,pageSize);
-			return new ResponseMessage(MessageCode.SUCCESS, result);
-		} catch (Exception e){ 
-			return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
-		}
-	}
-	 
-    @RequestMapping(value = "/append", method = RequestMethod.POST)
-    public ResponseMessage addIncome(Income income){
-        try { 
-        	if(income.getUserId() == null){
-        		income.setUserId(getUser()!= null?getUser().getId(): 1);
-        	}
-        	int i=incomeService.append(income);
-            return new ResponseMessage(MessageCode.SUCCESS,i);
-        } catch (Exception e){ 
+    @ApiOperation(value = "查询年收入详情", notes = "查询菜单列表")
+    @GetMapping(value = "/incomeDetails")
+    public ResponseMessage incomeDetails(Income income, Integer pageNo, Integer pageSize) {
+        try {
+            if (income.getYear() == null) {
+                income.setYear(Integer.valueOf(DateUtil.getSysYear()));
+            }
+            PageBean<Income> result = incomeService.incomeDetails(income, pageNo, pageSize);
+            return new ResponseMessage(MessageCode.SUCCESS, result);
+        } catch (Exception e) {
+            return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
+        }
+    }
+
+    @PostMapping("/append")
+    public ResponseMessage addIncome(Income income) {
+        try {
+            if (income.getUserId() == null) {
+                income.setUserId(getUser() != null ? getUser().getId() : 1);
+            }
+            int i = incomeService.append(income);
+            return new ResponseMessage(MessageCode.SUCCESS, i);
+        } catch (Exception e) {
             return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
         }
     }
 
     //修改
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public ResponseMessage updateIncome(Income income){
-        try { 
+    @PostMapping("/update")
+    public ResponseMessage updateIncome(Income income) {
+        try {
             incomeService.update(income);
             return new ResponseMessage(MessageCode.SUCCESS);
-        } catch (Exception e){ 
+        } catch (Exception e) {
             return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
         }
     }
 
     //删除
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public ResponseMessage delIncome(int id){
-        try { 
+    @PostMapping("/delete")
+    public ResponseMessage delIncome(int id) {
+        try {
             incomeService.delete(id);
             return new ResponseMessage(MessageCode.SUCCESS);
-        } catch (Exception e){ 
+        } catch (Exception e) {
             return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
         }
     }
-    
-	@ApiOperation(value = "纯收入", notes = "纯收入")
-	@RequestMapping(value = "/netIncome", method = RequestMethod.GET)
-	public ResponseMessage netIncome(int year){
-		try {
-			 HashMap<String, Object>  result = incomeService.netIncome( year,getUser().getId());
-			return new ResponseMessage(MessageCode.SUCCESS, result);
-		} catch (Exception e){ 
-			return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
-		}
-	}
+
+    @ApiOperation(value = "纯收入", notes = "纯收入")
+    @GetMapping(value = "/netIncome")
+    public ResponseMessage netIncome(int year) {
+        try {
+            HashMap<String, Object> result = incomeService.netIncome(year, getUser().getId());
+            return new ResponseMessage(MessageCode.SUCCESS, result);
+        } catch (Exception e) {
+            return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
+        }
+    }
 
 }
 
--- src/main/java/cn/xiaoyu/controller/tally/SummaryController.java
+++ src/main/java/cn/xiaoyu/controller/tally/SummaryController.java
@@ -1,10 +1,4 @@
 package cn.xiaoyu.controller.tally;
-import java.util.HashMap;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import cn.xiaoyu.common.Base;
 import cn.xiaoyu.common.MessageCode;
@@ -12,59 +6,68 @@ import cn.xiaoyu.common.ResponseMessage;
 import cn.xiaoyu.entity.tally.Summary;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * 描述:
- * @author 
- * 日期:2018-08-07
+ *
+ * @author 日期:2018-08-07
  * 日期:2018-12-20 删掉没用接口
-*/ 
+ */
 @Api(value = "summary", description = "汇总信息")
 @RestController
 @RequestMapping("/summary")
 public class SummaryController extends Base {
 
-	
-	@ApiOperation(value = "年度报表每日消费总和数据", notes = "年度报表每日消费总和数据")
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public ResponseMessage list(int year){
-		try { 
-			List<Summary> list=summaryService.list(year,getUser().getId());
-			return new ResponseMessage(MessageCode.SUCCESS,list);
-		} catch (Exception e){ 
-			return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
-		}
-	}
-	@ApiOperation(value = "季节综合报表", notes = "（季节总消费， 模糊查询 房租，服装，餐饮 +消费统计）")
-	@RequestMapping(value = "/seasonComprehensive", method = RequestMethod.GET)
-	public ResponseMessage seasonComprehensive(int year){
-		try { 
-			HashMap<String,Object> list= summaryService.seasonComprehensive(year,getUser().getId());
-			return new ResponseMessage(MessageCode.SUCCESS,list);
-		} catch (Exception e){ 
-			return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
-		}
-	}
-	
-	@ApiOperation(value = "月度综合报表", notes = "（月收入，月支出，月平均消费）")
-	@RequestMapping(value = "/monthlyComprehensive", method = RequestMethod.GET)
-	public ResponseMessage monthlyComprehensive(int year){
-		try { 
-			HashMap<String,List<String>> returnMap= summaryService.monthlyComprehensive(year,getUser().getId());
-			return new ResponseMessage(MessageCode.SUCCESS,returnMap);
-		} catch (Exception e){ 
-			return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
-		}
-	}
-	@ApiOperation(value = "周综合报表", notes = "（月收入，月支出，月平均消费）")
-	@RequestMapping(value = "/annualWeeklyReport", method = RequestMethod.GET)
-	public ResponseMessage annualWeeklyReport(){
-		try { 
-			HashMap<String,Object> returnMap= summaryService.annualWeeklyReport(getUser().getId());
-			return new ResponseMessage(MessageCode.SUCCESS,returnMap);
-		} catch (Exception e){ 
-			return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
-		}
-	}
-    
+
+    @ApiOperation(value = "年度报表每日消费总和数据", notes = "年度报表每日消费总和数据")
+    @GetMapping(value = "/list")
+    public ResponseMessage list(int year) {
+        try {
+            List<Summary> list = summaryService.list(year, getUser().getId());
+            return new ResponseMessage(MessageCode.SUCCESS, list);
+        } catch (Exception e) {
+            return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
+        }
+    }
+
+    @ApiOperation(value = "季节综合报表", notes = "（季节总消费， 模糊查询 房租，服装，餐饮 +消费统计）")
+    @GetMapping(value = "/seasonComprehensive")
+    public ResponseMessage seasonComprehensive(int year) {
+        try {
+            HashMap<String, Object> list = summaryService.seasonComprehensive(year, getUser().getId());
+            return new ResponseMessage(MessageCode.SUCCESS, list);
+        } catch (Exception e) {
+            return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
+        }
+    }
+
+    @ApiOperation(value = "月度综合报表", notes = "（月收入，月支出，月平均消费）")
+    @GetMapping(value = "/monthlyComprehensive")
+    public ResponseMessage monthlyComprehensive(int year) {
+        try {
+            HashMap<String, List<String>> returnMap = summaryService.monthlyComprehensive(year, getUser().getId());
+            return new ResponseMessage(MessageCode.SUCCESS, returnMap);
+        } catch (Exception e) {
+            return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
+        }
+    }
+
+    @ApiOperation(value = "周综合报表", notes = "（月收入，月支出，月平均消费）")
+    @GetMapping(value = "/annualWeeklyReport")
+    public ResponseMessage annualWeeklyReport() {
+        try {
+            HashMap<String, Object> returnMap = summaryService.annualWeeklyReport(getUser().getId());
+            return new ResponseMessage(MessageCode.SUCCESS, returnMap);
+        } catch (Exception e) {
+            return new ResponseMessage(MessageCode.UNKNOWN_ERROR, e.getMessage());
+        }
+    }
+
 }
 
